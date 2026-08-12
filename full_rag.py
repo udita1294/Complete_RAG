1b86e6f913a73723fa4e238291c93a939b81f030
Udita Singh <udita1294@gmail.com>
2026-08-12 21:09:43 +0530

    function for estimating cosine similarity created

--- full_rag.py
+++ full_rag.py
@@ -1,1 +1,42 @@
-s
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+client = Groq(api_key=my_api_key)
+model = "llama-3.3-70b-versatile"
+
+documents = [
+    "Employees receive 24 days of paid leave per year.",
+   
+    "Employees work from the office on Tuesday, Wednesday and Thursday. "
+    "Monday and Friday are optional work-from-home days.",
+   
+    "Employees receive Rs 3000 per month for gym reimbursement.",
+   
+    "Employees can claim Rs 2000 per month for home internet.",
+   
+    "Employees have a 90 day notice period."
+]
+
+document_embeddings = model.encode(documents)
+
+def cosine_similarity(a,b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+
+
+query = "How much vacation do I get?"
+query_embedding = model.encode(query)
+
